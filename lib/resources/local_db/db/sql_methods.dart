2902365f476560e2ddfccb9157c63054370d6884
jazper298 <minatosjutsu@gmail.com>
2020-07-29 11:57:43 +0800

    Sqlite CRUD in flutter

--- lib/resources/local_db/db/sql_methods.dart
+++ lib/resources/local_db/db/sql_methods.dart
@@ -1,37 +1,113 @@
+import 'dart:io';
+
 import 'package:flutterskypeclone/models/log.dart';
 import 'package:flutterskypeclone/resources/local_db/interface/log_interface.dart';
+import 'package:path_provider/path_provider.dart';
+import 'package:sqflite/sqflite.dart';
+import 'package:path/path.dart';
 
 class SqliteMethods implements LogInterface{
-  @override
-  addLogs(Log log) {
-    // TODO: implement addLogs
-    print("Adding values to Sqlite db");
-    return null;
+
+  Database _db;
+
+  String databaseName = "LogDB";
+
+  String tableName = "Call_Logs";
+
+  //columns
+  String logId = 'log_id';
+  String callerName = 'caller_name';
+  String callerPic = 'caller_pic';
+  String receiverName = 'receiver_name';
+  String receiverPic  = 'receiver_pic';
+  String callStatus = 'call_status';
+  String timestamp = 'timestamp';
+
+  Future<Database> get db async {
+    if (_db != null){
+      return _db;
+    }
+    print("db was null, now waiting it");
+    _db = await init();
+    return _db;
   }
 
   @override
-  close() {
-    // TODO: implement close
-    return null;
+  init() async {
+    Directory directory = await getApplicationDocumentsDirectory();
+    String path = join(directory.path, databaseName);
+    var db = await openDatabase(path, version: 1, onCreate: _onCreate);
+    return db;
+  }
+
+  _onCreate(Database db, int version) async {
+    String createTableQuery = "CREATE TABLE $tableName ($logId INTEGER PRIMARY KEY, $callerName TEXT, $callerPic TEXT, "
+        "$receiverName TEXT, $receiverPic TEXT, $callStatus TEXT, $timestamp TEXT)";
+
+    await db.execute(createTableQuery);
+    print("table created");
   }
 
   @override
-  deleteLogs(int logId){
-    // TODO: implement deleteLogs
-    return null;
+  addLogs(Log log) async {
+    var dbClient = await db;
+    await dbClient.insert(tableName, log.toMap(log));
   }
 
   @override
-  Future<List<Log>> getLogs() {
-    // TODO: implement getLogs
-    return null;
+  deleteLogs(int logId) async{
+    var dbClient = await db;
+    return await dbClient.delete(tableName, where: '$logId = ?', whereArgs: [logId]);
+  }
+
+  updateLogs(Log log) async {
+    var dbClient = await db;
+
+    await dbClient.update(
+      tableName,
+      log.toMap(log),
+      where: '$logId = ?',
+      whereArgs: [log.logId],
+    );
   }
 
   @override
-  init() {
-    // TODO: implement init
-    print("Initialized sqlite db");
-    return null;
+  Future<List<Log>> getLogs() async {
+    try{
+      var dbClient = await db;
+
+      //List<Map> maps = await dbClient.rawQuery("SELECT * FROM $tableName");
+      List<Map> maps = await dbClient.query(
+        tableName,
+        columns: [
+          logId,
+          callerName,
+          callerPic,
+          receiverName,
+          receiverPic,
+          callStatus,
+          timestamp
+        ]
+      );
+
+      List<Log> logList = [];
+
+      if(maps.isNotEmpty){
+        for(Map map in maps){
+          logList.add(Log.fromMap(map));
+        }
+      }
+      return logList;
+    }catch(e){
+      print(e);
+      return null;
+    }
+  }
+
+  @override
+  close() async{
+    var dbClient = await db;
+    dbClient.close();
   }
 
 }
